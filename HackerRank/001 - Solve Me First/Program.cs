6b76ebcc3657688d3f897920acc58651829b16ba
Mustafa Masoud <moatasemmasoud5@gmail.com>
2026-07-08 18:16:08 +0300

    Solving first problem in hacker-rank

---
+++ HackerRank/001 - Solve Me First/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace No._1;
+
+class Program
+{
+    public static int SolveMeFirst(int number1 , int number2)=> number1 + number2;
+    static void Main(string[] args)
+    {
+        Console.Write("Enter Value1 : ");
+        int value1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter Value2 : ");
+        int value2 = Convert.ToInt32(Console.ReadLine());
+
+        int result = SolveMeFirst(value1,value2);
+        Console.WriteLine($"The Sum is : {result}");
+    }
+}
